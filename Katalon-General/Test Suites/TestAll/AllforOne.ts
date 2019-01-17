ce7ebf96110f9b7fc197db90011cb801a0e8eb77
Awanden <bryan_awan@yahoo.com>
2019-01-17 10:01:42 +0700

    First Commit - Initial Project
    add some barebones to the project

---
+++ Katalon-General/Test Suites/TestAll/AllforOne.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AllforOne</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>1ec3beb1-7d4a-4ecd-9921-cf613ecf6c0b</testSuiteGuid>
+   <testCaseLink>
+      <guid>549bffd4-ad29-4ec9-8444-e707a1bb9ba2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Scenarios/ContohUpload/UploadSendkeys</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
